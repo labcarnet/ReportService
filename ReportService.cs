2771eb941fba137bfed49c2985b8384e280c92b8
labcarnet <labcar@labcar.pl>
2021-04-19 12:11:41 +0200

    Add project files.

---
+++ ReportService.cs
@@ -0,0 +1,130 @@
+﻿using Cipher;
+using EmailSender;
+using ReportService.Core.Models;
+using ReportService.Core.Repositories;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ReportService
+{
+    public partial class ReportService : ServiceBase
+    {
+        private Timer _timer;
+        private ErrorRepository _errorRepository = new ErrorRepository();
+        private ReportRepository _reportRepository = new ReportRepository();
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private Email _email;
+        private GenerateHtmlEmail _htmlEmail = new GenerateHtmlEmail();
+        private string _emailReceiver;
+        private int _sendHours;
+        private int _intervalInMinutes;
+        private bool _sendReportEnable;
+        private StringCipher _stringCipher = new StringCipher("804B8AA7-4ABA-4957-AB78-4C36EEBE0431");
+        private const string NotEncryptedPasswordPrefix = "encrypt:";
+
+
+        public ReportService()
+        {
+            InitializeComponent();
+
+            try
+            {
+                _emailReceiver = ConfigurationManager.AppSettings["ReceiverEmail"];
+                _sendHours = Convert.ToInt32(ConfigurationManager.AppSettings["SendHours"]);
+                _intervalInMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalInMinutes"]);
+                _sendReportEnable = Convert.ToBoolean(ConfigurationManager.AppSettings["SendReportEnable"]);
+
+                _timer = new Timer(_intervalInMinutes * 60000);
+
+                _email = new Email(new EmailParams
+                {
+                    HostSmtp = ConfigurationManager.AppSettings["HostSmtp"],
+                    Port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]),
+                    EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]),
+                    SenderName = ConfigurationManager.AppSettings["SenderName"],
+                    SenderEmail = ConfigurationManager.AppSettings["SenderEmail"],
+                    SenderEmailPassword = DecryptSenderEmialPassword()
+                });
+            }
+            catch(Exception ex)
+            {
+                Logger.Error(ex, ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string DecryptSenderEmialPassword()
+        {
+            var encryptedPassword = ConfigurationManager.AppSettings["SenderEmailPassword"];
+            if (encryptedPassword.StartsWith(NotEncryptedPasswordPrefix))
+            {
+                encryptedPassword = _stringCipher
+                    .Encrypt(encryptedPassword.Replace(NotEncryptedPasswordPrefix, ""));
+
+                var configFile = ConfigurationManager.OpenExeConfiguration
+                    (ConfigurationUserLevel.None);
+                configFile.AppSettings.Settings["SenderEmailPassword"].Value = encryptedPassword;
+                configFile.Save(); 
+            }
+            return _stringCipher.Decrypt(encryptedPassword);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer.Elapsed += DoWork;
+            _timer.Start();
+            Logger.Info("Service started...");
+        }
+
+        private async void DoWork (object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                await SendError();
+                if(_sendReportEnable) await SendReport();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async Task SendError()
+        {
+            var errors = _errorRepository.GetLastErrors(_intervalInMinutes);
+
+            if (errors == null || !errors.Any())
+                return;
+
+            await _email.Send("Błędy w aplikacji", _htmlEmail.GenerateErrors(errors, _intervalInMinutes), _emailReceiver);
+
+            Logger.Info("Error sent.");
+        }
+
+        private async Task SendReport()
+        {
+            var actualHour = DateTime.Now.Hour;
+            if (actualHour < _sendHours) return;
+
+            var report = _reportRepository.GetLastNotSentReport();
+            if (report == null)
+                return;
+
+            await _email.Send("Raport dobowy", _htmlEmail.GenerateReport(report), _emailReceiver);
+
+            _reportRepository.ReportSent(report);
+
+            Logger.Info("Report sent.");
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Info("Service stopped...");
+        }
+    }
+}
